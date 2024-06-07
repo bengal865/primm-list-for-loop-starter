6468c9dd2cb7af7d94f5a79dc61a62456cedd5aa
bengal865 <69059209+bengal865@users.noreply.github.com>
2024-06-07 13:53:59 -0400

    Add files via upload

---
+++ main.py
@@ -0,0 +1,10 @@
+candy_list = ["Snickers", "Twix", "Butterfinger", "Starburst", "Gummi Bears"]
+total_candy = 0
+
+# Part of the code inside the for loop is missing!  
+
+for candy in candy_list:
+    # What will happen inside the loop?
+    
+
+
